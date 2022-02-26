76ac81ee92bc64666ce5e255743072271caa484d
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-02-26 13:39:31 -0800

    refactor to mark "private" variables and functions with underscore

--- client.py
+++ client.py
@@ -4,36 +4,37 @@ import datetime
 
 
 class Client:
-    client_id = None
-    client_secret = None
-    client_creds_b64 = None
-    access_token = None
-    access_token_expires = datetime.datetime.now()
+    _client_id = None
+    _client_secret = None
+    _client_creds_b64 = None
+    _access_token = None
+    _access_token_expires = datetime.datetime.now()
 
     def __init__(self, client_id: str, client_secret: str):
-        self.client_id = client_id
-        self.client_secret = client_secret
+        self._client_id = client_id
+        self._client_secret = client_secret
         creds = f"{client_id}:{client_secret}"
-        self.client_creds_b64 = base64.b64encode(creds.encode())
+        self._client_creds_b64 = base64.b64encode(creds.encode())
 
-    def check_access_token_expiry(self):
+    def check_access_token_expiry(self) -> bool:
         now = datetime.datetime.now()
-        return self.access_token_expires < now
+        return self._access_token_expires < now
 
     def get_access_token(self):
         if self.check_access_token_expiry():
-            self.auth()
-        return self.access_token
+            if not self._auth():
+                exit(1)
+        return self._access_token
 
-    def auth(self):
-        if self.client_id is None:
+    def _auth(self):
+        if self._client_id is None:
             return False
         token_url = "https://accounts.spotify.com/api/token"
         token_data = {
             "grant_type": "client_credentials"
         }
         token_header = {
-            "Authorization": f"Basic {self.client_creds_b64.decode()}"
+            "Authorization": f"Basic {self._client_creds_b64.decode()}"
         }
         r = requests.post(token_url, data=token_data, headers=token_header)
         data = r.json()
@@ -42,8 +43,8 @@ class Client:
             print("auth error")
             return False
 
-        self.access_token = data['access_token']
+        self._access_token = data['access_token']
         now = datetime.datetime.now()
         expires_in = data['expires_in']
-        self.access_token_expires = now + datetime.timedelta(seconds=expires_in)
+        self._access_token_expires = now + datetime.timedelta(seconds=expires_in)
         return True
