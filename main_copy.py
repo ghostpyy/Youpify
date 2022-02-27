aef37c29e63ca87270217b7b042bd2f384461159
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-02-27 02:22:19 -0800

    not working lol

--- main_copy.py
+++ main_copy.py
@@ -1,7 +1,13 @@
+import time
+
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
+
+SLEEP_INTERVAL = 1
+TOP_TRACK_CNT = 5
 
 
 def spotify_init():
@@ -37,21 +43,32 @@ def youtube_init():
     )
 
 
-def main():
-    sp = spotify_init()
-    spotify_request = sp.current_user_top_tracks(20, 0, "medium_term")
+def get_top_spotify(sp, track_cnt: int):
+    spotify_request = sp.current_user_top_tracks(track_cnt, 0, "medium_term")
     top_tracks = []
     for track in spotify_request['items']:
         track_name = track['name']
         track_artist_name = track['artists'][0]['name']
         top_tracks.append((track_name, track_artist_name))
+    return top_tracks
 
-    yt = youtube_init()
+
+def get_tracks_spotify_playlist(sp, playlist_id):
+    tracks = []
+    playlist_tracks = sp.playlist_items(playlist_id=playlist_id, additional_types=["track"])
+    for track in playlist_tracks['items']:
+        track_name = track['track']['name']
+        track_artist_name = track['track']['artists'][0]['name']
+        tracks.append((track_name, track_artist_name))
+    return tracks
+
+
+def create_new_yt_playlist(yt, playlist_title):
     create_playlist = yt.playlists().insert(
         part="snippet,status",
         body={
             "snippet": {
-                "title": "Top Spotify tracks playlist",
+                "title": playlist_title,
                 "description": "Created for UCI Hackathon 2022",
                 "defaultLanguage": "en"
             },
@@ -61,27 +78,56 @@ def main():
         }
     )
     response_create_playlist = create_playlist.execute()
-    print(response_create_playlist)
+    return response_create_playlist['id']
+
+
+def add_to_yt_playlist(yt, playlist_id, top_tracks):
     for track in top_tracks:
         request_song = yt.search().list(
             part="snippet",
             maxResults=1,
             q=track[0] + ' ' + track[1]
         )
+        time.sleep(SLEEP_INTERVAL)
         song_response = request_song.execute()
+        video_id = song_response['items'][0]['id']['videoId']
+
         add_video_request = yt.playlistItems().insert(
             part="snippet,status",
             body={
                 'snippet': {
-                    'playlistId': response_create_playlist['id'],
+                    'playlistId': playlist_id,
                     'resourceId': {
                         'kind': 'youtube#video',
-                        'videoId': song_response['items'][0]['id']['videoId']
+                        'videoId': video_id
                     }
                 }
             }
         )
-        add_video_request.execute()
+        time.sleep(SLEEP_INTERVAL)
+        response = add_video_request.execute()
+
+
+def conv_playlist_spotify_youtube(sp, yt, playlist_id):
+    tracks = get_tracks_spotify_playlist(sp, playlist_id)
+    yt_playlist_id = create_new_yt_playlist(yt, "Spotify Playlist")
+    add_to_yt_playlist(yt, yt_playlist_id, tracks)
+
+
+def main():
+    sp = spotify_init()
+    yt = youtube_init()
+
+    # convert a spotify playlist to a YouTube playlist
+    # playlist_url = 'https://open.spotify.com/playlist/5FvzeSjMVp0xYt9loCuP8A'
+    playlist_url = 'https://open.spotify.com/playlist/4sjcxbRlA4E4cb9XgxozsB'
+    playlist_id = playlist_url.split('/')[-1]
+    conv_playlist_spotify_youtube(sp, yt, playlist_id)
+
+    # convert top spotify tracks to a YouTube playlist
+    # top_tracks = get_top_spotify(sp, TOP_TRACK_CNT)
+    # new_playlist_id = create_new_yt_playlist(yt, "Top Spotify Tracks")
+    # add_to_yt_playlist(yt, new_playlist_id, top_tracks)
 
 
 if __name__ == '__main__':
