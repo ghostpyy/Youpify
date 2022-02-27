eb03fc5c06f1b21051e9ec1e8f2628be1156ce5d
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-02-26 18:20:50 -0800

    rewrite using spotipy library

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-import requests
-
-import client
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
 def main():
-    f = open("client_info.txt")
-    client_id = f.readline().strip()
-    client_secret = f.readline().strip()
-    # print(client_id, client_secret)
-    test_client = client.Client(client_id, client_secret)
-    token = test_client.get_access_token()
-    # print(token)
-
-    # token won't be returned if the query window is too early
-    # in that case, use previous value
-    if token is None:
-        token = f.readline().strip()
-        f.close()
-    else:
-        f.close()
-        w = open('client_info.txt', 'w')
-        w.write(client_id + '\n' + client_secret + '\n' + token)
-        w.close()
-
-    test_playlist_link = 'https://open.spotify.com/playlist/3j8LBB0fzGoyCjutd91Te6'
-    test_playlist_code = test_playlist_link.split("/")[-1]
-    # print(test_playlist_code)
-    url = f"https://api.spotify.com/v1/playlists/{test_playlist_code}/tracks"
-    header = {
-        "Authorization": f"Bearer {token}"
-    }
-    r = requests.get(url, headers=header)
-    data = r.json()
-    for track in data['items']:
-        print(track['track']['name'])
+    scope = 'user-top-read'
+    f = open('client_info2.txt', 'r')
+    CLIENT_ID = f.readline().strip()
+    CLIENT_SECRET = f.readline().strip()
+    REDIRECT_URI = f.readline().strip()
+    f.close()
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            redirect_uri=REDIRECT_URI,
+            scope=scope
+        )
+    )
+    r = sp.current_user_top_tracks()
+    for track in r['items']:
+        print(track['name'], track['artists'][0]['name'], sep=' - ')
 
 
 if __name__ == '__main__':
