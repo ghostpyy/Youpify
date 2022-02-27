ecfac1813af10e83e1218cee5e85f12ec663fea3
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-02-26 18:31:24 -0800

    a

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
-def main():
+def spotify_init():
     scope = 'user-top-read'
     f = open('client_info2.txt', 'r')
     CLIENT_ID = f.readline().strip()
     CLIENT_SECRET = f.readline().strip()
     REDIRECT_URI = f.readline().strip()
     f.close()
-    sp = spotipy.Spotify(
+    return spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             client_id=CLIENT_ID,
             client_secret=CLIENT_SECRET,
@@ -17,9 +17,16 @@ def main():
             scope=scope
         )
     )
-    r = sp.current_user_top_tracks()
+
+
+def main():
+    sp = spotify_init()
+    r = sp.current_user_top_tracks(20, 0, "medium_term")
+    d = []
     for track in r['items']:
-        print(track['name'], track['artists'][0]['name'], sep=' - ')
+        track_name = track['name']
+        track_artist_name = track['artists'][0]['name']
+        print(track_name, track_artist_name, sep=' - ')
 
 
 if __name__ == '__main__':
